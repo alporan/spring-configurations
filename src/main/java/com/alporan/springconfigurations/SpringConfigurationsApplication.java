6c1667640636abfadf22c35ad3ac6108966f7d80
alporan <alporan@windowslive.com>
2020-08-31 22:06:04 +0300

    Initial commit

---
+++ src/main/java/com/alporan/springconfigurations/SpringConfigurationsApplication.java
@@ -0,0 +1,13 @@
+package com.alporan.springconfigurations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigurationsApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigurationsApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/alporan/springconfigurations/SpringConfigurationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alporan.springconfigurations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConfigurationsApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
