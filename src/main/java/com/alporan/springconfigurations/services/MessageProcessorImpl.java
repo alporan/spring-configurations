eb65ffd8412bdc9efdcdf6b2ed952d1e8092357a
alporan <alporan@windowslive.com>
2020-09-03 21:45:02 +0300

    Annotation based configuration. Closes 2

--- src/main/java/com/alporan/springconfigurations/services/MessageProcessorImpl.java
+++ src/main/java/com/alporan/springconfigurations/services/MessageProcessorImpl.java
@@ -1,11 +1,19 @@
 package com.alporan.springconfigurations.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Required;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 public class MessageProcessorImpl implements MessageProcessor {
 	
 	private String message;
 	private MessageService messageService;
 	
-	public MessageProcessorImpl(MessageService messageService) {
+	@Autowired
+	public MessageProcessorImpl(@Qualifier("emailService") MessageService messageService) {
 		this.messageService = messageService;
 	}
 	
@@ -15,15 +23,18 @@ public class MessageProcessorImpl implements MessageProcessor {
 	}
 	
 	@Override
+	@PostConstruct
 	public void initBean() {
 		messageService.sendMessage("Initialized");
 	}
 	
 	@Override
+	@PreDestroy
 	public void destroyBean() {
 		messageService.sendMessage("Destroyed");
 	}
 	
+	@Required
 	public void setMessage(String message) {
 		this.message = message;
 	}
